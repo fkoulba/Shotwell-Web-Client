0e6f51530ae98a9ab1055b6127ffcf539150b3f7
Fyodor Koulba <fyodor@koulba.net>
2013-11-16 15:26:37 +0400

    Initialized new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShotwellWebClient::Application.config.secret_key_base = '15c7ef06721062b419e133b1a814ddd6e91e8263c357a66aa7c28c1578b8f66c89f7d319a1e8d51bf244d1c9ea2640a1805541b12b4e5c2507e28c11f7860e15'
