7a36a72dbeacfb2cc9f130e9e2fb08bc68dcd97f
Fyodor Koulba <fyodor@koulba.net>
2013-12-04 00:02:13 +0400

    Fixed loca thumbnail paths

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,13 +5,12 @@ class Photo < ShotwellTable
 
   def thumbnail_filename
     local_filename = 'thumbs360/thumb' + self.id.to_s(16).rjust(16, '0') + '.jpg'
-    if File.exists?(Rails.root.join('data/shotwell/thumbs/', local_filename))
-      Rails.root.join('data/shotwell/thumbs/', local_filename)
-    elsif File.exists?(Rails.root.join('data', local_filename))
-      Rails.root.join('data', local_filename)
+
+    if File.exists?(path = Rails.root.join('data/shotwell/thumbs/', local_filename))
+      path
     else
-      `convert "#{filename}" -thumbnail 480x360^ -gravity center -unsharp 0x.5 -extent 480x360 -auto-orient "#{Rails.root.join('data', local_filename)}"`
-      Rails.root.join('data', local_filename)
+      `convert "#{filename}" -thumbnail 480x360^ -gravity center -unsharp 0x.5 -extent 480x360 -auto-orient "#{Rails.root.join('data/local', local_filename)}"` unless File.exists?(path = Rails.root.join('data/local', local_filename))
+      path
     end
   end
 
